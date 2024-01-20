7db4da3a65ebecdcbbeffc3413b844ce19ebc0a6
Jonatandb <jonatandb@gmail.com>
2024-01-20 15:20:52 -0300

    Add admin site custom header

--- core_poll/settings.py
+++ core_poll/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'core_poll.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
